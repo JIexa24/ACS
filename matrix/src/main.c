0a62b8e96a2d51ec7714577ec3a25935ca0a62e3
Alexey R <lexa260011@ngs.ru>
2017-11-15 23:16:02 +0700

    Update main.c

--- matrix/src/main.c
+++ matrix/src/main.c
@@ -159,7 +159,7 @@ int main(int argc, char** argv)
   }
   
   printf("needlvl %d\n",needlevel);
-  dat datat = {rezult,one, two,realSize,realSize, 1, needlevel};
+  dat datat = {rezult,one, two,realSize,realSize, 1, needlevel, 0};
   time = wtime();
    simpleMatrixProizvCacheObliviousp(&datat);
 //  simpleMatrixProizvCacheOblivious(rezult, one, two, realSize, realSize);
